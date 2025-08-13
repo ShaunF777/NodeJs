505f0becc2cf6e0c1df2477d352f0f9ab3205b18
ShaunF777 <shaunfr777@gmail.com>
2025-08-13 15:26:16 +0200

    Implement basic HTTP server with overview, product, and API routes in simpleAPI.js

---
+++ starter/simpleAPI.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////
+// FILES
+
+
+///////////////////////
+// SERVER
+
+
+// Create Server
+const server = http.createServer((req, res) => {
+    //console.log(req.url);
+    const pathName = req.url;
+
+    if(pathName === '/overview') {
+        res.end('This is the overview route');
+    } else if (pathName === '/product') {
+        res.end('This is the product route');
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf8', (err,data) => {
+            const productData = JSON.parse(data);
+            console.log(productData);
+            res.end('API');
+        })
+    } else {
+        res.writeHead(404, { // Never send headers after the response
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+        
+});
+
+// Listen for requests on port, host ip
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
