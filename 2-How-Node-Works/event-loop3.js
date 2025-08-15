c424a20a160c391f8e916204f4f3736d40b8d24c
ShaunF777 <shaunfr777@gmail.com>
2025-08-15 15:04:54 +0200

    use the eventEmitter  to showcase the Observer pattern , also how the emitter can update pass argument values

---
+++ 2-How-Node-Works/event-loop3.js
@@ -0,0 +1,37 @@
+/*1. Write a bunch of code.
+2. Try to figue out in witch order they should be executed in the EVENT LOOP.
+3. Then analyse if the results we thereafter get actualy makes sense.*/
+const fs = require('fs');
+const crypto = require('crypto');
+process.env.UV_THREADPOOL_SIZE = 2;
+
+const start = Date.now();
+
+setTimeout(() => console.log('Timer 1 finished'), 0);
+setImmediate(() => console.log('Immediate 1 finished'));
+
+fs.readFile('test-file.txt', () => {
+  console.log('I/O finished');
+  console.log('------------------------------------------');
+  setTimeout(() => console.log('Timer 2 finished'), 0);
+  setTimeout(() => console.log('Timer 3 finished'), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('Process.nextTic'));
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+});
+
+console.log('Hello from the top level code');
+
+/*Blocks the entire execution and background workers could only be run after the blockage */
---
+++ 2-How-Node-Works/events.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events'); // Standard name for aquring this bult in events class
+
+const myEmitter = new EventEmitter(); // Create new instance or object of the created class
+
+// Observer pattern. Multiple listeners can be setup that wait & listen for the same events
+myEmitter.on('newSale', () => { // Listener 1
+  console.log('There was a new sale!');
+});
+
+myEmitter.on('newSale', () => { // Listener 2
+  console.log('Customer name: Jonas');
+});
+
+myEmitter.on('newSale', stock => { // Listener 3 
+  console.log(`There are now ${stock} items left in stock.`);
+}); // multiple listeners for the same event will execute synchronously
+
+myEmitter.emit('newSale', 9); // This object emits the pattern
