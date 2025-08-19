051db689f8af065e69cfebbc01f2a9e045a489eb
ShaunF777 <shaunfr777@gmail.com>
2025-08-19 13:20:08 +0200

    Chained the .then and .catch methods to the .get method

--- 3-Asynchronous-Js/index3.js
+++ 3-Asynchronous-Js/index3.js
@@ -4,7 +4,8 @@
  * 3. Then save that random image to another text file
  * Its a 3 spep process all of which involves callbacks, that will show the problem we get with these
  */
-// NOW WE WILL BE CONSUMING PROMISES, RATHER THAN NESTED CALLBACKS
+// NOW WE WILL BE CONSUMING PROMISES, RATHER THAN NESTED CALLBACKS. 
+// We will chain the .then and .catch methods to the .get promise
 
 const superagent = require('superagent');
 const fs = require('fs');
@@ -12,15 +13,22 @@ const fs = require('fs');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
   superagent
-    // get method will return a promise even though it's pending (NO data yet)
+    // get method will return a promise even though it's a 'PENDING' promise (NO data yet)
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      if (err) return console.log(err.message);
+    .then((res) => { 
+      // then method only handles fulfilled promises, and needs .catch for the error
       console.log(res.body);
-
+      // when the data does come through it's a 'RESOLVED' promise
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log('Random dog image saved to file!');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
+/**A RESOLVED promise can either be a 'FULFILLED' promise, or when its a 'REJECTED' promise
+ * So 'FULFILLED' has a 'RESOLVED' we can use.
+ * But 'REJECTED' will have an error that needs to be handled like 'if (err) return console.log(err.message);'
+ */
