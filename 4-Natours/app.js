e3f6d865fa673232da99214c6d35a4c894a75545
ShaunF777 <shaunfr777@gmail.com>
2025-08-22 18:45:05 +0200

    Update testPostamanApi50.js name

--- 4-Natours/app.js
+++ 4-Natours/app.js
@@ -1,15 +1,27 @@
-// By convention, app.js is used to hold the Express app setup and configuration
 const express = require('express');
+const fs = require('fs');
 
-const app = express();
-// Calling express() creates an application object
 // This "app" has methods to define routes, middleware, and configuration
+const app = express(); // Calling express() creates an application object
+
+// Run once and parse Json data into an array of javascript objects kept for later use inside tours
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 // -------------------------
 // ROUTES
 // -------------------------
 // Good practice to keep versions, for updates and testing updates
-app.get('/api/v1/tours', (req, res) => {});
+app.get('/api/v1/tours', (req, res) => {
+  // route handler req, res will reply with the a list of tour data
+  res.status(200).json({
+    status: 'success',
+    results: tours.length, // For client info when sending arrays
+    data: {
+      // Normally tours(key route) : tours(value object)
+      tours, // ES6 way if key and value has the same name
+    },
+  });
+});
 
 // -------------------------
 // START SERVER
