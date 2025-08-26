7911ba6c2e9b650f6b425eefa3d3fd478144ca23
ShaunF777 <shaunfr777@gmail.com>
2025-08-26 20:19:51 +0200

    Add to app.js the ability to serve static files withing the public folder

--- 4-Natours/app.js
+++ 4-Natours/app.js
@@ -5,14 +5,15 @@ const express = require('express');
 const morgan = require('morgan'); // Logs routing feedback in console
 
 // Import the routers that imports their handler modules from controllers folder
-const tourRouter = require('./routes/tourRoutes') // imports the whole file
-const userRouter = require('./routes/userRoutes') 
+const tourRouter = require('./routes/tourRoutes'); // imports the whole file
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // --- GLOBAL CONFIGURATION (middleware) ---
-app.use(express.json()); // 'use' Adds a function to our middleware stack
 app.use(morgan('dev')); // https://expressjs.com/en/resources/middleware.html
+app.use(express.json()); // 'use' Adds a function to our middleware stack
+app.use(express.static(`${__dirname}/public`)); // Serves static files from a file 
 
 app.use((req, res, next) => {
   console.log('👉Hello from the middleware👈');
@@ -29,7 +30,7 @@ app.use((req, res, next) => {
 // 2. Depenting on the route and request, it will execute one of the controller functions
 
 // ---ROUTES middleware---
-app.use('/api/v1/tours', tourRouter); // Mount subapplication for tourRouter 
-app.use('/api/v1/users', userRouter); // Mount subapplication for userRouter 
+app.use('/api/v1/tours', tourRouter); // Mount subapplication for tourRouter
+app.use('/api/v1/users', userRouter); // Mount subapplication for userRouter
 
 module.exports = app; // Allow export of the whole application configuration in one file
