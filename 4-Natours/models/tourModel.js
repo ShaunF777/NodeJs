030540ba540a812ec3b2abae6c470974644a1922
ShaunF777 <shaunfr777@gmail.com>
2025-09-02 18:21:58 +0200

    Refactor schema and model creation into a new models folder, and tourModels.js file. Remove the testing code we used to test our connection to the remote DB

---
+++ 4-Natours/models/tourModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+// Everything concerning models need to be in this folder and file
+// Specify a schema for our data with schema-type-options for validations or default values
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true, // Disallows two tour documents with the same name
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Create a model of the tourSchema schema. Model names to be capitalized.
+const Tour = mongoose.model('Tour', tourSchema); // (model name, schema name)
--- 4-Natours/server.js
+++ 4-Natours/server.js
@@ -11,7 +11,7 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
-
+// Connect to the database
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -22,50 +22,6 @@ mongoose
     //console.log(con.connections); // to show everything inside the connections object
     console.log('DB connection successful!');
   });
-
-//console.log(process.env); // Env variables loaded onto the node process
-//console.log(app.get('env')); // Global environment variable used by Express to define the env that the node app is running in
-
-// Specify a schema for our data with schema-type-options for validations or default values
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true, // Disallows two tour documents with the same name
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Create a model of the tourSchema schema. Model names to be capitalized.
-const Tour = mongoose.model('Tour', tourSchema); // (model name, schema name)
-
-// Remember that models are almost like classes.
-// Create a new document out of the model. testTour is now an instance of the model
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💢: ', err);
-  });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
 
 // --- ROUTE FLOW goes like this ---
 // 1. We recieve the request here. Then it goes to app.js
